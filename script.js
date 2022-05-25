eb7830a412a07a7832fef941a51b4886cb7099af
Federico Krenn <fedekrenn@gmail.com>
2022-05-25 16:54:02 -0300

    Se añaden botones y funcionalidad para parar el reloj

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
+// Obtengo los botones del DOM
+const botonInicio = document.getElementById("inicio")
+const botonParar = document.getElementById("stop")
+
+// Evento para iniciar el contador
+botonInicio.addEventListener("click", iniciar)
+
+// Se inicializan variables
+let cronometro;
 let segundos = 0;
 let minutos = 0;
 let imprimirSegundos;
 let imprimirMinutos;
 
-function actualizarContador() {
+
+// Función para iniciar el cronómetro
+function iniciar(){
+
+    // Despliegue de la actualización, con intervalo de 1 segundo
+    cronometro = setInterval(actualizarReloj, 1000)
+    
+    // escucha de botón parar para lanzar la función que rompe el intervalo
+    botonParar.addEventListener("click", parar)
+
+    function parar(){
+        clearInterval(cronometro)
+    }
+
+}
+
+
+function actualizarReloj() {
     segundos++;
 
     if (segundos > 59) {
         segundos = 00;
         minutos++
     }
+    
     imprimirMinutos = minutos;
     imprimirSegundos = segundos;
 
@@ -28,4 +55,3 @@ function actualizarContador() {
 
 
 
-setInterval(actualizarContador, 1000)
